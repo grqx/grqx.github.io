7454cd8c8079f8cfcdb6788b398062dd6ff98ae8
grqx_wsl <173253225+grqx@users.noreply.github.com>
2024-06-28 17:39:48 +1200

    first commit

---
+++ jsErrorTest/nk1pH5.js
@@ -0,0 +1,9 @@
+if (new URLSearchParams(window.location.search).get("IKnowThisIsRickRoll") == 'true') {
+    console.log("I'll probably let you go...");
+} else {
+    alert("Are you ready?");
+    function IdeV4X() {
+        document.location.replace("https://youtu.be/dQw4w9WgXcQ");  
+    };
+    setTimeout(IdeV4X, 250);
+}
