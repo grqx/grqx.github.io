f28ce2438ac7ddb9c53cdbf6e285380ad179bc7e
grqx <173253225+grqx@users.noreply.github.com>
2024-12-17 13:12:06 +1300

    add page jumpToBing

---
+++ jumpToBing/RX6PIz.js
@@ -0,0 +1,1 @@
+function exiGGS() {return const xtIDJM = new URLSearchParams(window.location.search), const ECPBPh = parseInt(xtIDJM.get("timeout")) || 3000, const lQnAcn = xtIDJM.get("url") || "https://www.bing.com/", setTimeout(()=>{window.location.replace(lQnAcn);}, ECPBPh);} document.addEventListener("DOMContentLoaded", exiGGS);
